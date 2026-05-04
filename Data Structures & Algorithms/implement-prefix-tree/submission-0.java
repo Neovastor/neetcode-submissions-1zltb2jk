fd7f7f435306cd61e6c1b37aba81420799dedc4c
Brian Dwijayanto <78195733+Neovastor@users.noreply.github.com>
2026-05-04 23:49:38 +0700

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.java
@@ -0,0 +1,67 @@
+class PrefixTree {
+    Tree root;
+
+
+    public PrefixTree() {
+        this.root = new Tree();
+    }
+
+    public void insert(String word) {
+        Tree cur = this.root;
+        for (int i = 0; i < word.length(); i++){
+            char ch = word.charAt(i);
+            if (cur.map.containsKey(ch)){
+                cur = cur.map.get(ch);
+            } else {
+                Tree newTree = new Tree(ch);
+                cur.map.put(ch, newTree);
+                cur = cur.map.get(ch);
+
+            }
+        }
+        cur.isWord = true;
+    }
+
+    public boolean search(String word) {
+        Tree cur = this.root;
+
+        for (int i = 0; i < word.length(); i++){
+            char ch = word.charAt(i);
+            if (cur.map.containsKey(ch)){
+                cur = cur.map.get(ch);
+            } else {
+                return false;
+            }
+        }
+        return cur.isWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        Tree cur = this.root;
+
+        for (int i = 0; i < prefix.length(); i++){
+            char ch = prefix.charAt(i);
+            if (cur.map.containsKey(ch)){
+                cur = cur.map.get(ch);
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+class Tree {
+    public char value;
+    public boolean isWord;
+    public Map<Character, Tree> map = new HashMap<>();
+
+    public Tree(char val){
+        this.value = val;
+    }
+
+    public Tree(){
+    }
+
+}
+
